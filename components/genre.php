fc964eac9bc7650801a53b54212e3127fb01eaa2
yu-kitano <ktn.ysk.1996@gmail.com>
2020-03-12 10:40:50 +0000

    ジャンルエリアサンプル2

--- components/genre.php
+++ components/genre.php
@@ -1,9 +1,9 @@
 <div class="uk-visible@m sm-genre-area uk-margin-right">
   <div class="uk-flex uk-flex-center selector">
     <select name="sources" id="genre-selector" class="custom-select sources" placeholder="ジャンル">
-      <option value="profile">Profile</option>
-      <option value="word">Word</option>
-      <option value="hashtag">Hashtag</option>
+      <option value="capture">攻略本</option>
+      <option value="reference">参考書</option>
+      <option value="another">その他</option>
     </select>
   </div>
 </div>
