a855fdc97e01062d348a53d7561bd94cbe24f5b4
yu-kitano <ktn.ysk.1996@gmail.com>
2020-03-14 19:09:57 +0900

    ジャンル切り替えのjs作成

--- components/genre.php
+++ components/genre.php
@@ -1,9 +1,9 @@
 <div class="uk-visible@m sm-genre-area uk-margin-medium-right">
   <div class="uk-flex uk-flex-center uk-margin-large-top">
     <select name="sources" id="genre-selector" class="custom-select sources" placeholder="全ジャンル">
-      <option value="capture">攻略本</option>
-      <option value="reference">参考書</option>
-      <option value="another">その他</option>
+      <option value="genre-capture">攻略本</option>
+      <option value="genre-reference">参考書</option>
+      <option value="genre-another">その他</option>
     </select>
   </div>
 </div>
--- components/genreSearch.php
+++ components/genreSearch.php
@@ -1,43 +1,89 @@
 <div class="uk-card uk-card-default uk-card-body uk-margin-bottom uk-padding-remove-horizontal uk-padding-remove-bottom sm-book-area uk-flex uk-flex-column">
-  <div class="uk-flex uk-flex-left uk-margin-large-left sm-genre-search-books">
-    <div class="uk-flex">
-      <div class="uk-card uk-card-default uk-card-body uk-padding-remove sm-book uk-height-small uk-margin-right">
-        本表紙
-      </div>
-      <div class="uk-flex uk-flex-column uk-flex-between">
-        <div class="uk-margin-bottom">タイトル</div>
-        <div class="uk-margin-bottom">
-          <p class="sm-synopsis">いいえ彼はトムです。</p>
+  <div class="uk-flex uk-flex-column book-genre-select" id="genre-all">
+    <!-- ジャンル詳細 -->
+    <div class="uk-flex uk-flex-left uk-margin-large-left sm-genre-search-books">
+      <div class="uk-flex">
+        <div class="uk-card uk-card-default uk-card-body uk-padding-remove sm-book uk-height-small uk-margin-right">
+          本表紙
         </div>
-        <div class="uk-margin-auto-left">
-          <button class="uk-button uk-button-default">作品ページへ</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="sm-divider"></div>
-  <!-- ループにする-->
-  <div class="uk-flex uk-flex-left uk-margin-large-left sm-genre-search-books">
-    <div class="uk-flex">
-      <div class="uk-card uk-card-default uk-card-body uk-padding-remove sm-book uk-height-small uk-margin-right">
-        本表紙
-      </div>
-      <div class="uk-flex uk-flex-column uk-flex-between">
-        <div class="uk-margin-bottom">タイトル2</div>
-        <div class="uk-margin-bottom">
-          <p class="sm-synopsis">
-            彼がトムなわけがないだろう。何故なら彼は10年前にペンと間違えられたショックで死んでしまっているんだ。
-          </p>
-        </div>
-        <div class="uk-margin-auto-left">
-          <button class="uk-button uk-button-default">作品ページへ</button>
+        <div class="uk-flex uk-flex-column uk-flex-between">
+          <div class="uk-margin-bottom">タイトル</div>
+          <div class="uk-margin-bottom">
+            <p class="sm-synopsis">いいえ彼はトムです。</p>
+          </div>
+          <div class="uk-margin-auto-left">
+            <button class="uk-button uk-button-default">作品ページへ</button>
+          </div>
         </div>
       </div>
     </div>
+    <div class="sm-divider"></div>
+    <!-- ループにする-->
+    <div class="uk-flex uk-flex-left uk-margin-large-left sm-genre-search-books">
+      <div class="uk-flex">
+        <div class="uk-card uk-card-default uk-card-body uk-padding-remove sm-book uk-height-small uk-margin-right">
+          本表紙
+        </div>
+        <div class="uk-flex uk-flex-column uk-flex-between">
+          <div class="uk-margin-bottom">タイトル2</div>
+          <div class="uk-margin-bottom">
+            <p class="sm-synopsis">
+              彼がトムなわけがないだろう。何故なら彼は10年前にペンと間違えられたショックで死んでしまっているんだ。
+            </p>
+          </div>
+          <div class="uk-margin-auto-left">
+            <button class="uk-button uk-button-default">作品ページへ</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sm-divider"></div>
   </div>
-  <div class="sm-divider"></div>
-  <!-- ループの最後の要素だ↓後に詳細リンクを作る -->
+
+  <div class="uk-flex uk-flex-column book-genre uk-hidden" id="genre-capture">
+    <!-- ジャンル詳細 -->
+    <div class="uk-flex uk-flex-left uk-margin-large-left sm-genre-search-books">
+      <div class="uk-flex">
+        <div class="uk-card uk-card-default uk-card-body uk-padding-remove sm-book uk-height-small uk-margin-right">
+          本表紙
+        </div>
+        <div class="uk-flex uk-flex-column uk-flex-between">
+          <div class="uk-margin-bottom">タイトル</div>
+          <div class="uk-margin-bottom">
+            <p class="sm-synopsis">しかしトムは虎視眈々と復讐の機会を探っていたのだ…今トムのリベンジが始まる</p>
+          </div>
+          <div class="uk-margin-auto-left">
+            <button class="uk-button uk-button-default">作品ページへ</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sm-divider"></div>
+  </div>
+
+  <div class="uk-flex uk-flex-column book-genre uk-hidden" id="genre-reference">
+    <!-- ジャンル詳細 -->
+    <div class="uk-flex uk-flex-left uk-margin-large-left sm-genre-search-books">
+      <div class="uk-flex">
+        <div class="uk-card uk-card-default uk-card-body uk-padding-remove sm-book uk-height-small uk-margin-right">
+          本表紙
+        </div>
+        <div class="uk-flex uk-flex-column uk-flex-between">
+          <div class="uk-margin-bottom">タイトル</div>
+          <div class="uk-margin-bottom">
+            <p class="sm-synopsis">トムは激怒した。必ずや邪知暴虐の王を撃ち倒さんと決意した</p>
+          </div>
+          <div class="uk-margin-auto-left">
+            <button class="uk-button uk-button-default">作品ページへ</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sm-divider"></div>
+  </div>
+
+  <!-- ループの最後を出力した後に詳細リンクを作る -->
   <div class="uk-margin-right uk-margin-auto-left">
-    <a class="uk-button uk-padding-remove uk-width-1-1" href="">ジャンル一覧へ</a>
+    <a class="uk-button uk-padding-remove uk-width-1-1" href="">ジャンル詳細へ</a>
   </div>
 </div>
